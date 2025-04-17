87f36ca16e5396632509d25c22da1ef3b296a453
AJ <142957187+alecd124@users.noreply.github.com>
2025-04-17 15:03:39 -0700

    Update hit.js

--- hit.js
+++ hit.js
@@ -1,1 +1,28 @@
-
+function playerHit() {
+  const card = deck.pop();
+  playerHand.push(card);  
+  playerSum = handVal(playerHand);
+  if (playerSum == 21) {
+    status = "won";
+  }
+  else if (sum > 21) {
+    status = "lost, player bust"; 
+}
+function dealerHit() {
+  const card = deck.popcard(); 
+  dealerHand.push(card); 
+  dealerSum = handVal(dealerHand); 
+  while (dealerSum < 17) { // dealer hits aslong as <17 
+    dealerHand.push(card); 
+    dealerSum = handVal(dealerHand); 
+  }
+  if (dealerSum == playerSum) { // tie game 
+    status = "tie"; 
+  }
+  else if (dealerSum > 21) { // dealer busted 
+    status = "won, dealer bust"; 
+  }
+  else if (playerSum < aiSum) { // if dealers hand is stronger than players 
+    status = "player lost"; 
+  }
+}
