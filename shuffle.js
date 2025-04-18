a84f1d7ec1069e954613ed092b37a6d300c98258
bryab-edu <156254217+bryab-edu@users.noreply.github.com>
2025-04-17 21:23:43 -0700

    Update and rename shuffle.js to Deck.js

--- shuffle.js
+++
@@ -1,6 +0,0 @@
-function shuffle (deck) {
-  for (let i = deck.length - 1; i > 0; i--) { 
-    let j = Math.floor(Math.random() * (i + 1)); 
-       [deck[i], deck[j]] = [deck[j], deck[i]]; 
-    } 
-}
